1cd096409a50ea130d894db25a6737f25bbbab7d
Shreyan Das <hrijudas@hotmail.com>
2022-04-26 21:08:02 +0530

    Exercises 1-4 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+  ans = ''
+  if (number < 0) {
+    return "ERROR"
+  }
 
+  for (let i = 0; i < number; i++) {
+    ans += string
+  }
+
+  return ans
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(ar, ...args) {
+    let arr = ar 
 
+    for (let i = 0; i < args.length; i++) {
+        const element = args[i]
+        if (arr.includes(element)) {
+        arr = arr.filter(ele => ele !== element )
+        }      
+    }
+    return arr
 };
 
+console.log(removeFromArray([1,2,3,4],1,2,3))
 // Do not edit below this line
 module.exports = removeFromArray;
