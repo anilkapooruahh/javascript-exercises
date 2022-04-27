31a1cdc5e1950795ca3c3b50d0a20f07171495c7
Shreyan Das <hrijudas@hotmail.com>
2022-04-27 17:54:44 +0530

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const round = num => Math.round(Math.round(num * 100) /10) /10 
+
+const ftoc = function(temp) {
+  const result = (temp - 32) * (5/9)
+  //Rounding to one decimal place
+  return round(result)
 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const result = (temp * (9/5)) + 32
+  return round(result)
 };
 
 // Do not edit below this line
