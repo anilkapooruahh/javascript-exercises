c3e0c0e7800a6eb745094ecec87335589d23627c
Shreyan Das <hrijudas@hotmail.com>
2022-04-27 17:06:58 +0530

    6 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    const s = start < end ? start : end
+    const e = end < start ? start : end
+    if (typeof(s) !== 'number' || s < 0) {
+        return 'ERROR'
+    }
+    if (typeof(e) !== 'number' || e < 0) {
+        return 'ERROR'
+    }
+    let ans = 0
+    for (let i = s; i < (e + 1); i++) {
+        ans += i
+    }
+    return ans
 };
 
 // Do not edit below this line
