0f9b4908a027652ceef05e950cac9abe4693d208
Shreyan Das <hrijudas@hotmail.com>
2022-04-28 20:12:18 +0530

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  //string manipulation
+  const cleanString = string.split('').filter(char => !(char.search(/[^A-Za-z]/) != -1)).join('').toLowerCase()
+
+  return cleanString ===  cleanString.split('').reverse().join('')
 
 };
 
