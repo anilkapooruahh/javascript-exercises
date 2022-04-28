2ed7e239d34010a512ecd68cd170240854d07be7
Shreyan Das <hrijudas@hotmail.com>
2022-04-28 21:51:57 +0530

    11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const help = (prev, next) => {
+  if (prev['yearOfDeath'] === undefined) {
+    prev['yearOfDeath'] = Date().slice(11,15) - 0
+  }  
+  if (next['yearOfDeath'] === undefined) {
+    next['yearOfDeath'] = Date().slice(11,15) - 0
+  }
+  
+  return (prev['yearOfDeath'] - prev['yearOfBirth']) < (next['yearOfDeath'] - next['yearOfBirth']) ? next : prev;
+   
+}  
+const findTheOldest = function(people) {
+  const oldest = people.reduce(help)
 
+  return oldest
 };
 
 // Do not edit below this line
